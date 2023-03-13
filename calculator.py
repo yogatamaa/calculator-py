3e4c0fe6c31c520407058f66cca3acc3d2690788
yogatamaa <yogatama105@gmail.com>
2023-03-13 22:33:03 +0700

    s3

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ class Calculator:
         self.window = tk.Tk()
         self.window.geometry("375x667")
         self.window.resizable(0, 0)
-        self.window.title("Brantor")
+        self.window.title("caltor")
 
         self.total_expression = ""
         self.current_expression = ""
